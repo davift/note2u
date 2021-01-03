07eedaa9c1c325c775bc30ff387a5653c991ac6d
davift <73488542+davift@users.noreply.github.com>
2021-01-03 11:09:48 -0500

    Update new.php

--- http/new.php
+++ http/new.php
@@ -17,7 +17,7 @@ file_put_contents("../data/$noteid/pass", md5($password));
 
       $token = $datetime."\tAuthor\tNote Created";
       $cipher_method = 'aes-128-ctr';
-      $enc_key = openssl_digest(md5($password), 'SHA256', TRUE);
+      $enc_key = openssl_digest(sha1($password), 'SHA256', TRUE);
       $enc_iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher_method));
       $cryptedtoken = openssl_encrypt($token, $cipher_method, $enc_key, 0, $enc_iv) . "::" . bin2hex($enc_iv);
       unset($token, $cipher_method, $enc_key, $enc_iv);
