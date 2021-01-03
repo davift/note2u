daf27b49a2f0eb3d239d26910582faa866bb38fe
davift <73488542+davift@users.noreply.github.com>
2021-01-03 12:52:43 -0500

    Add files via upload

---
+++ http/statistics.php
@@ -0,0 +1,19 @@
+<?php
+// Comment the following line to enable the statistics about the usage of this website.
+exit;
+
+$path = '../data';
+$bytestotal = 0;
+$path = realpath($path);
+if($path!==false && $path!='' && file_exists($path)){
+  foreach(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS)) as $object){
+    $bytestotal += $object->getSize();
+  }
+}
+$bytestotal = $bytestotal/1000000;
+
+echo "Usage: $bytestotal MB<br>";
+echo 'Unique: '.file_get_contents("../unique", true).'<br>';
+echo 'New: '.file_get_contents("../new", true).'<br>';
+echo 'Open: '.file_get_contents("../open", true).'<br>';
+?>
